1b3091c1d5588cdb97381cbfeca987d9318b5121
Fay Carsons <faycarsons23@gmail.com>
2024-02-02 21:43:18 -0500

    first commit, working but not general

---
+++ example-blog.md
@@ -0,0 +1,11 @@
+Date: December 29, 2023
+
+# My Blog
+
+This is an example blog post about my **Rust** *blog parser*. It converts a markdown file into **HTML**, adds *attributes* defined in a local *JSON* file(of which there is an example in this repo), then encapsulates the HTML in a *JSON object* with all the relevant fields so I can easily serve and then parse my blog posts on he client side with **minimal runtime cost**. It also writes a Typescript file with a `Post` type declaration and an array of the title of each post, for easy fetching.
+
+It uses the `Markdown` rust crate and therefore supports all valid markdown syntax.
+
+The Date must be specified in he first line of the file, in the format `Date: month day, year`. The next line should be a header. Anything can follow that though!
+
+
---
+++ src/main.rs
@@ -0,0 +1,165 @@
+use chrono::NaiveDate;
+use clap::Parser;
+use std::{
+    fs,
+    path::PathBuf,
+};
+
+use std::collections::HashMap;
+use serde_json::from_str;
+
+type Attribute<'a> = &'a str;
+type Tag<'a> = &'a str;
+type ReplacementMap<'a> = HashMap<Tag<'a>, HashMap<Attribute<'a>, &'a str>>;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // Input file or directory
+    #[arg(short, long)]
+    input: PathBuf,
+
+    #[arg(long = "js")]
+    js_path: Option<PathBuf>,
+
+    // Output directory (has to be a directory)
+    #[arg(short, long)]
+    output: PathBuf,
+
+    #[arg(short, long)]
+    replacements: PathBuf,
+}
+
+#[derive(Clone, Debug)]
+struct InternalPost {
+    title: String,
+    date: NaiveDate,
+    header: String,
+    body: String,
+}
+
+#[derive(serde::Serialize)]
+struct Post {
+    title: String,
+    date: String,
+    header: String,
+    body: String,
+}
+
+impl From<InternalPost> for Post {
+    fn from(
+        InternalPost {
+            title,
+            date,
+            header,
+            body,
+        }: InternalPost,
+    ) -> Self {
+        Post {
+            title,
+            date: date.format("%B %d, %Y").to_string(),
+            header,
+            body,
+        }
+    }
+}
+
+const POST_TYPE: &str =
+    "export type Post = {name: string, date: string, header: string, content: string[]}";
+
+fn replace(repl: &ReplacementMap, html: &str) -> String {
+    let mut buffer = html.to_owned();
+    for (elt, map) in repl.iter() {
+        let tag = format!("<{elt}>");
+        println!("{tag}");
+        while buffer.contains(&tag) {
+            let attributes = map
+                .iter()
+                .map(|(attr, val)| format!("{attr}=\"{val}\" "))
+                .collect::<String>();
+            buffer = buffer.replace(&tag, &format!("{} {}>", &tag[..tag.len() - 1], attributes));
+        }
+    }
+
+    buffer
+}
+
+fn main() -> std::io::Result<()> {
+    let Args {
+        input,
+        js_path,
+        output,
+        replacements,
+    } = Args::parse();
+    let mut titles = Vec::<String>::new();
+    let mut post_objects = Vec::<InternalPost>::new();
+
+    let md = if input.is_dir() {
+        fs::read_dir(input)?
+            .map(|entry| {
+                let entry = entry.unwrap();
+                entry.path()
+            })
+            .collect::<Vec<PathBuf>>()
+    } else {
+        vec![input]
+    };
+
+    if !output.exists() {
+        fs::create_dir(&output)?;
+    }
+
+    let json = fs::read_to_string(&replacements).unwrap_or_else(|_| panic!("JSON replacement map {} does not exist",
+        replacements.display()));
+    let replacement_map =
+        from_str::<ReplacementMap>(&json).expect("Invalid structure in JSON replacement map: ");
+
+    for path in md {
+        let filename = path
+            .file_name()
+            .unwrap_or_else(|| panic!("Error in file {:?}", path));
+        let content = fs::read_to_string(&path)?;
+        let (date, rest) = content.split_once('\n').unwrap_or_else(|| panic!("{} does not have a date in the first line",
+            filename.to_str().unwrap()));
+        let date = date.replace("Date: ", "");
+        let date = chrono::naive::NaiveDate::parse_from_str(date.trim(), "%B %d, %Y").unwrap_or_else(|_| panic!("Invalid date format in {}", filename.to_str().unwrap()));
+
+        let html = markdown::to_html(rest);
+        let with_classses = replace(&replacement_map, &html);
+
+        let (header, body) = with_classses.split_once('\n').unwrap_or_else(|| panic!("Blog {} does not have a title header",
+            filename.to_str().unwrap()));
+        let (header, body) = (header.to_owned(), body.to_owned());
+
+        let title = filename.to_str().unwrap().to_owned();
+
+        let post = InternalPost {
+            title,
+            date,
+            header,
+            body,
+        };
+        post_objects.push(post);
+    }
+
+    post_objects.sort_by_key(|post| post.date);
+
+    for post @ InternalPost { title, .. } in post_objects.iter() {
+        titles.push(format!("\'/{title}\', "));
+        let json = serde_json::to_string_pretty(&Post::from(post.clone()))
+            .unwrap_or_else(|_| panic!("Serializaion error in post: {title}"));
+        let path = output.join(format!("{title}.json"));
+        fs::write(path, json)?
+    }
+    
+    let titles_arr = serde_json::to_string_pretty(&titles).expect("Invalid title");
+    let ts_file = format!("{POST_TYPE}\n\n export posts: string[] = {titles_arr};");
+
+    let js = if let Some(path) = js_path {
+        path
+    } else {
+        output
+    }.join("posts.ts");
+
+    fs::write(js, ts_file)
+}
